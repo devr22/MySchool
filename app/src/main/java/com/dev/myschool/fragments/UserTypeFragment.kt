dd0e9874465e9e7131084a2f4c196090cc1eb9c9
devr22 <devr52222@gmail.com>
2021-08-28 11:35:30 +0530

    user roles bottom sheet

--- app/src/main/java/com/dev/myschool/fragments/UserTypeFragment.kt
+++ app/src/main/java/com/dev/myschool/fragments/UserTypeFragment.kt
@@ -1,16 +1,20 @@
 package com.dev.myschool.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.dev.myschool.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class UserTypeFragment : Fragment() {
 
     private lateinit var btnNext: Button
+    private lateinit var txtRoles: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,11 +29,16 @@ class UserTypeFragment : Fragment() {
             callSignupFragment()
         }
 
+        txtRoles.setOnClickListener {
+            showBottomSheetDialog(container!!.context)
+        }
+
         return view
     }
 
     private fun initViews(view: View) {
         btnNext = view.findViewById(R.id.userType_next_btn)
+        txtRoles = view.findViewById(R.id.userType_role_txt)
     }
 
     private fun callSignupFragment() {
@@ -37,6 +46,12 @@ class UserTypeFragment : Fragment() {
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.registration_fragment_container, signupFragment).addToBackStack(null)
             .commit()
+    }
+
+    private fun showBottomSheetDialog(context: Context) {
+        val bottomSheetDialog = BottomSheetDialog(context)
+        bottomSheetDialog.setContentView(R.layout.user_access_bottom_sheet)
+        bottomSheetDialog.show()
     }
 
 }
